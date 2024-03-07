4188750671781a147c1f1a74f0c3a0001bb5fa70
Aurellia <aurelliazza@gmail.com>
2024-03-07 11:39:44 +0700

    first commit

---
+++ src/main/java/config/env.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env {
+	protected WebDriver driver;
+	protected static String SDErpLink = "http://192.168.101.88:3033/login";
+}
